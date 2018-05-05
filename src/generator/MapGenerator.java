2710441d9da62843ebcab783290875a7ac833bb8
Peter Ziegler <pziegler97@gmail.com>
2018-05-06 01:16:15 +0200

    save and generate methods remain public

--- src/generator/MapGenerator.java
+++ src/generator/MapGenerator.java
@@ -39,7 +39,7 @@ public strictfp class MapGenerator {
 		}
 	}
 
-	private void save(String folderPath, String prefix, SCMap map, long seed) {
+	public void save(String folderPath, String prefix, SCMap map, long seed) {
 		try {
 			File file = new File(folderPath + prefix + "_" + seed);
 			file.mkdirs();
@@ -53,7 +53,7 @@ public strictfp class MapGenerator {
 		}
 	}
 
-	private SCMap generate(long seed) {
+	public SCMap generate(long seed) {
 		final Random random = new Random(seed);
 		final SCMap map = new SCMap(512, 6, 64, 10);
 
