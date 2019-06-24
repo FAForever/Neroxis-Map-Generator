6716af709b2a9e650162059650e110e79601069f
Rackover <rackover@racknet.noip.me>
2019-06-24 21:53:41 +0200

    Preview now uses gradient

--- src/java/generator/Preview.java
+++ src/java/generator/Preview.java
@@ -3,14 +3,29 @@ package generator;
 import map.BinaryMask;
 import map.FloatMask;
 import map.SCMap;
+import util.Gradient;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
 
 public strictfp class Preview {
 
-    static final float HEATMAP_DEADZONE = 0.1f;
-    static final int HEATMAP_GRADIENT_STEPS = 16;
+    static final float HEATMAP_DEADZONE = 0f;
+    static final int HEATMAP_GRADIENT_STEPS = 255;
+    static final Gradient HEATMAP_GRADIENT;
+    static final float HEATMAP_SATURATION = 0.8f;
+
+    static{
+        HEATMAP_GRADIENT = new Gradient();
+        HEATMAP_GRADIENT.addColor(0f, new Color(48, 48, 154)); // Dark magenta
+        HEATMAP_GRADIENT.addColor(0.33f, new Color(0, 153, 255)); // Pale blue
+        HEATMAP_GRADIENT.addColor(0.45f, new Color(0, 207, 104)); // Blueish green
+        HEATMAP_GRADIENT.addColor(0.6f, new Color(255, 255, 150)); // Chick yellow
+        HEATMAP_GRADIENT.addColor(0.8f, new Color(129, 92, 83)); // Brown
+        HEATMAP_GRADIENT.addColor(1f, new Color(255, 255, 255)); // White
+    }
 
     static void generate(BufferedImage image, SCMap map, FloatMask lightGrassTexture, BinaryMask rock, BinaryMask grass, BinaryMask lightGrass){
         for (int x = 0; x < 256; x++) {
@@ -23,28 +38,12 @@ public strictfp class Preview {
                         ((float)map.getHeightmap().getRaster().getSample(scaledX,scaledY,0))
                                 * SCMap.HEIGHTMAP_SCALE;
 
-                float delta = elevation * (1/50f); // Magic number to make the colors more readable
-                delta = (StrictMath.round(delta * HEATMAP_GRADIENT_STEPS + 0.5f) -0.5f) / HEATMAP_GRADIENT_STEPS;
-                delta = (1f - HEATMAP_DEADZONE) - delta * (1f - HEATMAP_DEADZONE * 2f);
+                float delta = elevation * (1/60f); // Magic number to make the colors more readable
 
-                float saturation = 0.9f;
-                float value = 0.5f;
-
-                saturation -= lightGrassTexture.get(x,y) * 0.4f; // Forests darken the preview a bit
-
-                if (rock.get(x,y)){
-                    delta *= 0.9f;
-                }
-                else if (
-                        !grass.get(x,y) &&
-                                !lightGrass.get(x,y) &&
-                                !rock.get(x,y)
-                ){
-                    value -= 0.1f;	// Lakes appear darker
-                }
-
-                Color color = Color.getHSBColor(delta, saturation, value);
-                image.setRGB(x, y, color.getRGB());
+                Color color = HEATMAP_GRADIENT.evaluate(delta);
+                float[] hsb = Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), null);
+                Color desaturated = Color.getHSBColor(hsb[0], hsb[1]*HEATMAP_SATURATION, hsb[2]);
+                image.setRGB(x, y, desaturated.getRGB());
             }
         }
     }
