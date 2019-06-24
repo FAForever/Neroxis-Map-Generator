380732aad5e46b35de0b87f4cec63ea0d3db75f5
Rackover <rackover@racknet.noip.me>
2019-06-25 01:58:41 +0200

    Generated maps can now use custom lighting too

--- src/java/biomes/Biome.java
+++ src/java/biomes/Biome.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import map.TerrainMaterials;
+import util.serialized.LightingSettings;
 import util.serialized.WaterSettings;
 
 @AllArgsConstructor
@@ -12,4 +13,5 @@ public strictfp class Biome {
     String name;
     TerrainMaterials terrainMaterials;
     WaterSettings waterSettings;
+    LightingSettings lightingSettings;
 }
---
+++ src/java/util/serialized/LightingSettings.java
@@ -0,0 +1,24 @@
+package util.serialized;
+
+import util.Vector3f;
+import util.Vector4f;
+
+import static map.SCMap.*;
+
+/** Used in disk operations to be converted into a material later
+ * Compliant with ozonex's SCMLighting format
+ * */
+
+public strictfp class LightingSettings {
+    public float LightingMultiplier = LIGHTING_MULTIPLIER;
+    public Vector3f SunDirection = SUN_DIRECTION;
+    public Vector3f SunAmbience = SUN_AMBIANCE_COLOR;
+    public Vector3f SunColor = SUN_COLOR;
+    public Vector3f ShadowFillColor = SHADOW_COLOR;
+    public Vector4f SpecularColor = SPECULAR_COLOR;
+    public float Bloom = BLOOM;
+    public Vector3f FogColor = FOG_COLOR;
+    public float FogStart = FOG_START;
+    public float FogEnd = FOG_END;
+
+}
