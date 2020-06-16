55b83020be714fe4f9ad62825f67e4d8dc8c3bca
Sheikh45 <Sheikh450@gmail.com>
2020-06-16 19:50:44 -0400

    Slightly increased appearance of navy factory wrecks

--- src/java/generator/MapGenerator.java
+++ src/java/generator/MapGenerator.java
@@ -182,7 +182,7 @@ public strictfp class MapGenerator {
 		BinaryMask t2NavyWreckMask = new BinaryMask(64, random.nextLong());
 		t2NavyWreckMask.randomize(0.01f).intersect(land.getBinaryMask().outline()).minus(noProps);
 		BinaryMask navyFactoryWreckMask = new BinaryMask(64, random.nextLong());
-		navyFactoryWreckMask.randomize(0.01f).minus(grass.getBinaryMask()).minus(noProps).deflate(5);
+		navyFactoryWreckMask.randomize(0.01f).minus(grass.getBinaryMask()).minus(noProps).deflate(2).trimEdge(10);
 		UnitGenerator unitGenerator = new UnitGenerator(map, random.nextLong());
 
 		wreckGenerator.generateWrecks(t1LandWreckMask, wreckGenerator.T1_Land, 2f);
