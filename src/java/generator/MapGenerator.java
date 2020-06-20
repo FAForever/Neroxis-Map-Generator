2de57d46ae5ef22290afc2a0e17488b34662c358
Sheikh45 <Sheikh450@gmail.com>
2020-06-20 16:02:22 -0400

    Bumped version number marker up to 1.0.0

--- src/java/generator/MapGenerator.java
+++ src/java/generator/MapGenerator.java
@@ -27,7 +27,7 @@ public strictfp class MapGenerator {
 
 	public static final boolean DEBUG = false;
 
-	public static final String VERSION = "0.1.6";
+	public static final String VERSION = "1.0.0";
 
 	//read from cli args
 	private static Random randomseed = new Random();
