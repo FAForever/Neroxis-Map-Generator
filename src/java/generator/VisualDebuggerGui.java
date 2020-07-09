f5104682b89f97c2ba54ba1557aa730f6e5b765b
Sheikah45 <66929319+Sheikah45@users.noreply.github.com>
2020-07-09 19:46:51 -0400

    Visual debugger updates (#65)
    * Sort Debugger list by mask name
    
    * Add step number to Mask name to enable step through

--- src/java/generator/VisualDebuggerGui.java
+++ src/java/generator/VisualDebuggerGui.java
@@ -11,95 +11,6 @@ import java.util.Map;
 public class VisualDebuggerGui {
 
     public static final boolean AUTO_SELECT_NEW_MASKS = false;
-    private static JFrame frame;
-    private static Container contentPane;
-    private static JList<MaskListItem> list;
-    private static JPanel canvasContainer;
-    private static final DefaultListModel<MaskListItem> listModel = new DefaultListModel<>();
-    private static final Map<String, ImagePanel> maskNameToCanvas = new HashMap<>();
-
-    public static boolean isCreated() {
-        return frame != null;
-    }
-
-    public static void createGui() {
-        if (frame != null) {
-            return;
-        }
-        frame = new JFrame();
-        contentPane = frame.getContentPane();
-        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
-
-        createList();
-        canvasContainer = new JPanel();
-        contentPane.add(canvasContainer);
-
-        frame.pack();
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    }
-
-    private static void createList() {
-        list = new JList<>(listModel);
-        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        list.addListSelectionListener(event -> {
-            if (!event.getValueIsAdjusting()) {
-                MaskListItem selectedItem = list.getSelectedValue();
-                onSelect(selectedItem.maskName);
-            }
-        });
-
-        JScrollPane listScroller = new JScrollPane(list);
-        listScroller.setMinimumSize(new Dimension(100, 0));
-        contentPane.add(listScroller);
-    }
-
-    public static void update(String uniqueMaskName, BufferedImage image, int zoomFactor) {
-        if (!uniqueMaskName.isEmpty()) {
-            if (!uniqueMaskName.equals("lastChanged")) {
-                int count = 0;
-                for (int i = 0; i < listModel.getSize(); i++) {
-                    if (listModel.get(i).maskName.matches(uniqueMaskName + ".*")) {
-                        count++;
-                    }
-                }
-                uniqueMaskName = uniqueMaskName + count;
-            }
-            boolean isNewMask = !maskNameToCanvas.containsKey(uniqueMaskName);
-            if (isNewMask) {
-                maskNameToCanvas.put(uniqueMaskName, new ImagePanel());
-                listModel.addElement(new MaskListItem(uniqueMaskName));
-            }
-            ImagePanel canvas = maskNameToCanvas.get(uniqueMaskName);
-            canvas.setViewModel(image, zoomFactor);
-            if (isNewMask && (listModel.getSize() == 1 || AUTO_SELECT_NEW_MASKS)) {
-                list.setSelectedIndex(listModel.getSize() - 1);
-            } else {
-                MaskListItem selected = list.getSelectedValue();
-                if (selected != null && selected.maskName.equals(uniqueMaskName)) {
-                    updateVisibleCanvas(uniqueMaskName, canvas);
-                }
-            }
-        }
-    }
-
-    public static void remove(String uniqueMaskName) {
-        listModel.removeElement(new MaskListItem(uniqueMaskName));
-    }
-
-    private static void onSelect(String uniqueMaskName) {
-        ImagePanel selectedCanvas = maskNameToCanvas.get(uniqueMaskName);
-        canvasContainer.removeAll();
-        canvasContainer.add(selectedCanvas);
-        updateVisibleCanvas(uniqueMaskName, selectedCanvas);
-    }
-
-    private static void updateVisibleCanvas(String maskName, ImagePanel canvas) {
-        canvas.revalidate();
-        canvas.repaint();
-        frame.pack();
-        frame.setTitle("Mask: " + maskName + ", Zoom: x" + canvas.getZoomFactor());
-    }
 
     /**
      * Panel that shows the given image.
@@ -153,5 +64,86 @@ public class VisualDebuggerGui {
         public String toString() {
             return "  " + maskName + "  ";
         }
+    }
+
+    private static JFrame frame;
+    private static Container contentPane;
+    private static JList<MaskListItem> list;
+    private static JPanel canvasContainer;
+
+    private static DefaultListModel<MaskListItem> listModel = new DefaultListModel<>();
+    private static Map<String, ImagePanel> maskNameToCanvas = new HashMap<>();
+
+    public static boolean isCreated() {
+        return frame != null;
+    }
+
+    public static void createGui() {
+        if (frame != null || !VisualDebugger.ENABLED) {
+            return;
+        }
+        frame = new JFrame();
+        contentPane = frame.getContentPane();
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
+
+        createList();
+        canvasContainer = new JPanel();
+        contentPane.add(canvasContainer);
+
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    private static void createList() {
+        list = new JList<>(listModel);
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.addListSelectionListener(event -> {
+            if (!event.getValueIsAdjusting()) {
+                MaskListItem selectedItem = list.getSelectedValue();
+                onSelect(selectedItem.maskName);
+            }
+        });
+
+        JScrollPane listScroller = new JScrollPane(list);
+        listScroller.setMinimumSize(new Dimension(100, 0));
+        contentPane.add(listScroller);
+    }
+
+    public static void update(String uniqueMaskName, BufferedImage image, int zoomFactor) {
+        if (!uniqueMaskName.isEmpty()) {
+            int ind = listModel.getSize();
+            int count = 0;
+            for (int i = 0; i < listModel.getSize(); i++) {
+                if (listModel.get(i).maskName.split(" ")[0].equals(uniqueMaskName.split(" ")[0])) {
+                    ind = i + 1;
+                    count += 1;
+                }
+            }
+            uniqueMaskName = String.format("%s %d", uniqueMaskName, count);
+            maskNameToCanvas.put(uniqueMaskName, new ImagePanel());
+            listModel.insertElementAt(new MaskListItem(uniqueMaskName), ind);
+            ImagePanel canvas = maskNameToCanvas.get(uniqueMaskName);
+            canvas.setViewModel(image, zoomFactor);
+            list.setSelectedIndex(0);
+        }
+    }
+
+    public static void remove(String uniqueMaskName) {
+        listModel.removeElement(new MaskListItem(uniqueMaskName));
+    }
+
+    private static void onSelect(String uniqueMaskName) {
+        ImagePanel selectedCanvas = maskNameToCanvas.get(uniqueMaskName);
+        canvasContainer.removeAll();
+        canvasContainer.add(selectedCanvas);
+        updateVisibleCanvas(uniqueMaskName, selectedCanvas);
+    }
+
+    private static void updateVisibleCanvas(String maskName, ImagePanel canvas) {
+        canvas.revalidate();
+        canvas.repaint();
+        frame.pack();
+        frame.setTitle("Mask: " + maskName + ", Zoom: x" + canvas.getZoomFactor());
     }
 }
