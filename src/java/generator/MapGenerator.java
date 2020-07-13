72e0eb0984c530cb2733e1c10f9b8ffc8ec4ba8b
Geosearchef <geosearchef@googlemail.com>
2020-07-13 11:52:11 +0200

    add timing debug statements to pipeline

--- src/java/generator/MapGenerator.java
+++ src/java/generator/MapGenerator.java
@@ -24,7 +24,7 @@ import java.util.*;
 
 public strictfp class MapGenerator {
 
-    public static final boolean DEBUG = false;
+    public static final boolean DEBUG = true;
     public static final String VERSION = "1.0.4";
 
     //read from cli args
