1e02360f16029c39e2101a67aa776ab78206254c
Sheikah45 <Sheikah450@gmail.com>
2020-07-16 00:22:50 -0400

    Update Version Number

--- src/java/generator/MapGenerator.java
+++ src/java/generator/MapGenerator.java
@@ -28,7 +28,7 @@ import java.util.concurrent.CompletableFuture;
 public strictfp class MapGenerator {
 
     public static final boolean DEBUG = true;
-    public static final String VERSION = "1.0.5";
+    public static final String VERSION = "1.0.6";
 
     //read from cli args
     private static String folderPath = ".";
