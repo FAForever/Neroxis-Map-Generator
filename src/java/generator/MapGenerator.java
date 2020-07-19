faf99ee499dd6a925b54d54af7192586ed51adb6
Sheikah45 <Sheikah450@gmail.com>
2020-07-19 10:42:17 -0400

    Update Version Number

--- src/java/generator/MapGenerator.java
+++ src/java/generator/MapGenerator.java
@@ -30,7 +30,7 @@ import java.util.concurrent.CompletableFuture;
 public strictfp class MapGenerator {
 
     public static final boolean DEBUG = false;
-    public static final String VERSION = "1.0.7";
+    public static final String VERSION = "1.0.8";
 
     public static final float MOUNTAIN_DENSITY_MAX = .075f;
     public static final float RAMP_DENSITY_MAX = .25f;
