3d0868ec11167da25d1e1e3f64ceb346185f37d1
Sheikah45 <Sheikah450@gmail.com>
2020-08-16 22:37:05 -0400

    Update Version Number

--- src/java/generator/MapGenerator.java
+++ src/java/generator/MapGenerator.java
@@ -31,7 +31,7 @@ import java.util.concurrent.CompletableFuture;
 public strictfp class MapGenerator {
 
     public static final boolean DEBUG = false;
-    public static final String VERSION = "1.0.13";
+    public static final String VERSION = "1.0.14";
     public static final BaseEncoding NAME_ENCODER = BaseEncoding.base32().omitPadding().lowerCase();
 
     public static final float LAND_DENSITY_MIN = .65f;
