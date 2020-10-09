bb355e12cf6667cb6cdac361574251764a72cecb
Sheikah45 <Sheikah450@gmail.com>
2020-10-09 07:42:17 -0400

    Use ozonex capitalization for serial setting

--- src/java/util/serialized/LightingSettings.java
+++ src/java/util/serialized/LightingSettings.java
@@ -4,8 +4,6 @@ import lombok.Data;
 import util.Vector3f;
 import util.Vector4f;
 
-import static map.SCMap.*;
-
 /**
  * Used in disk operations to be converted into a material later
  * Compliant with ozonex's SCMLighting format
@@ -13,15 +11,15 @@ import static map.SCMap.*;
 
 @Data
 public strictfp class LightingSettings {
-    private float LightingMultiplier = LIGHTING_MULTIPLIER;
-    private Vector3f SunDirection = SUN_DIRECTION;
-    private Vector3f SunAmbience = SUN_AMBIANCE_COLOR;
-    private Vector3f SunColor = SUN_COLOR;
-    private Vector3f ShadowFillColor = SHADOW_COLOR;
-    private Vector4f SpecularColor = SPECULAR_COLOR;
-    private float Bloom = BLOOM;
-    private Vector3f FogColor = FOG_COLOR;
-    private float FogStart = FOG_START;
-    private float FogEnd = FOG_END;
+    private float LightingMultiplier;
+    private Vector3f SunDirection;
+    private Vector3f SunAmbience;
+    private Vector3f SunColor;
+    private Vector3f ShadowFillColor;
+    private Vector4f SpecularColor;
+    private float Bloom;
+    private Vector3f FogColor;
+    private float FogStart;
+    private float FogEnd;
 
 }
--- src/java/util/serialized/WaterSettings.java
+++ src/java/util/serialized/WaterSettings.java
@@ -17,41 +17,41 @@ import static map.SCMap.*;
 
 @Data
 public strictfp class WaterSettings {
-    private boolean waterPresent = true;
-    private float elevation = WATER_HEIGHT;
-    private float elevationDeep = WATER_DEEP_HEIGHT;
-    private float elevationAbyss = WATER_ABYSS_HEIGHT;
+    private boolean WaterPresent;
+    private float Elevation;
+    private float ElevationDeep;
+    private float ElevationAbyss;
 
-    private Vector3f surfaceColor = WATER_SURFACE_COLOR;
-    private Vector2f colorLerp = WATER_COLOR_LERP;
-    private float refractionScale = WATER_REFRACTION;
-    private float fresnelBias = WATER_FRESNEL_BIAS;
-    private float fresnelPower = WATER_FRESNEL_POWER;
+    private Vector3f SurfaceColor;
+    private Vector2f ColorLerp;
+    private float RefractionScale;
+    private float FresnelBias;
+    private float FresnelPower;
 
-    private float unitReflection = WATER_UNIT_REFLECTION;
-    private float skyReflection = WATER_SKY_REFLECTION;
+    private float UnitReflection;
+    private float SkyReflection;
 
-    private float sunShininess = WATER_SUN_SHININESS;
-    private float sunStrength = WATER_SUN_STRENGH;
-    private Vector3f sunDirection = WATER_SUN_DIRECTION;
-    private Vector3f sunColor = WATER_SUN_COLOR;
-    private float sunReflection = WATER_SUN_REFLECTION;
-    private float sunGlow = WATER_SUN_GLOW;
+    private float SunShininess;
+    private float SunStrength;
+    private Vector3f SunDirection;
+    private Vector3f SunColor;
+    private float SunReflection;
+    private float SunGlow;
 
-    private String texPathCubemap = WATER_CUBEMAP_PATH;
-    private String texPathWaterRamp = WATER_RAMP_PATH;
+    private String TexPathCubemap;
+    private String TexPathWaterRamp;
 
-    private WaveTexture[] waveTextures = makeWaveTextures(WAVE_TEXTURE_PATHS, WAVE_NORMAL_REPEATS, WAVE_NORMAL_MOVEMENTS);
+    private WaveTexture[] WaveTextures = makeWaveTextures(WAVE_TEXTURE_PATHS, WAVE_NORMAL_REPEATS, WAVE_NORMAL_MOVEMENTS);
 
     private static WaveTexture[] makeWaveTextures(
-            String[] waveTexturePaths,
-            float[] waveNormalRepeats,
-            Vector2f[] waveNormalMovements) {
-        List<WaveTexture> texs = new LinkedList<>();
+            String[] WaveTexturePaths,
+            float[] WaveNormalRepeats,
+            Vector2f[] WaveNormalMovements) {
+        List<WaveTexture> textures = new LinkedList<>();
         for (int i = 0; i < WAVE_NORMAL_COUNT; i++) {
-            texs.add(new WaveTexture(waveTexturePaths[i], waveNormalMovements[i], waveNormalRepeats[i]));
+            textures.add(new WaveTexture(WaveTexturePaths[i], WaveNormalMovements[i], WaveNormalRepeats[i]));
         }
-        return texs.toArray(new WaveTexture[0]);
+        return textures.toArray(new WaveTexture[0]);
     }
 
     @Data
