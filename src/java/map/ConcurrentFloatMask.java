bea077f059433b46c5dbe0d08e11754ca8073daf
Emperor-Penguin1 <randompenguin27@gmail>
2020-10-15 14:00:41 -0400

    Substantially changed how all texture layers/masks are used (especially improving aesthetic qualities and slope representation)
    Substantially changed how all texture layers/masks are used/calculated/determined/placed (slopes are much better represented, mountains/hills/ramps/etc are much more precise, textures are generally smoothed/blended a lot more, and more texture layers are now used sufficiently and generally with much greater levels of detail and more aesthetic qualities)
    Removed ground texture layers/masks (layer 1)
    Changed layer 0 from showing water/beach to showing ground
    Moved texture layers/masks 2-6 down 1 layer each to now be layers 1-5
    Added waterBeach texture layers/masks in layer slot 6
    Added concurrent float/binary mask subtract functions (and imported visual debugger) to ConcurrentFloatMask.java
    Created new concurrent and regular binary masks for better calculating/using texture layers (they are inland, highGround, aboveBeach, aboveBeachEdge, flatAboveCoast, higherFlatAboveCoast, and lowWaterBeach)

--- src/java/map/ConcurrentFloatMask.java
+++ src/java/map/ConcurrentFloatMask.java
@@ -1,5 +1,6 @@
 package map;
 
+import generator.VisualDebugger;
 import lombok.Getter;
 import util.Pipeline;
 import util.Util;
@@ -59,14 +60,14 @@ public strictfp class ConcurrentFloatMask extends ConcurrentMask {
 
     public ConcurrentFloatMask subtract(ConcurrentFloatMask other) {
         return Pipeline.add(this, Arrays.asList(this, other), res ->
-                this.floatMask.subtract(((ConcurrentFloatMask) res.get(1)).getFloatMask())
-        );
+                this.floatMask.add(((ConcurrentFloatMask) res.get(1)).getFloatMask())
+        ).multiply(-1);
     }
 
     public ConcurrentFloatMask subtract(ConcurrentBinaryMask other, float value) {
         return Pipeline.add(this, Arrays.asList(this, other), res ->
-                this.floatMask.subtract(((ConcurrentBinaryMask) res.get(1)).getBinaryMask(), value)
-        );
+                this.floatMask.add(((ConcurrentBinaryMask) res.get(1)).getBinaryMask(), value)
+        ).multiply(-1);
     }
 
     public ConcurrentFloatMask multiply(float value) {
@@ -84,12 +85,6 @@ public strictfp class ConcurrentFloatMask extends ConcurrentMask {
     public ConcurrentFloatMask clampMin(float value) {
         return Pipeline.add(this, Collections.singletonList(this), res ->
                 this.floatMask.clampMin(value)
-        );
-    }
-
-    public ConcurrentFloatMask threshold(float value) {
-        return Pipeline.add(this, Collections.singletonList(this), res ->
-                this.floatMask.threshold(value)
         );
     }
 
