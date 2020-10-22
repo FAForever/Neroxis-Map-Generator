098fda3c7e055e5e0459b827dc6fa9fa4ea818fc
Sheikah45 <Sheikah450@gmail.com>
2020-10-22 09:38:19 -0400

    Fixes #215

--- src/java/generator/MapGenerator.java
+++ src/java/generator/MapGenerator.java
@@ -40,8 +40,8 @@ public strictfp class MapGenerator {
     public static final float MOUNTAIN_DENSITY_MIN = 0f;
     public static final float MOUNTAIN_DENSITY_MAX = 1f;
     public static final float MOUNTAIN_DENSITY_RANGE = MOUNTAIN_DENSITY_MAX - MOUNTAIN_DENSITY_MIN;
-    public static final float RAMP_DENSITY_MIN = .025f;
-    public static final float RAMP_DENSITY_MAX = .075f;
+    public static final float RAMP_DENSITY_MIN = .030f;
+    public static final float RAMP_DENSITY_MAX = .065f;
     public static final float RAMP_DENSITY_RANGE = RAMP_DENSITY_MAX - RAMP_DENSITY_MIN;
     public static final float PLATEAU_DENSITY_MIN = .45f;
     public static final float PLATEAU_DENSITY_MAX = .5f;
