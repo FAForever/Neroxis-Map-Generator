a541dda246da64f0d69f8b443e8c5feef9be2c41
Emperor-Penguin1 <randompenguin27@gmail>
2020-12-28 23:30:46 -0500

    Removed MaskImageWriter Class and added static methods based on it to the imageUtils class Created additional automatically scaled PNG writing functions

--- src/java/neroxis/util/ImageUtils.java
+++ src/java/neroxis/util/ImageUtils.java
@@ -1,11 +1,15 @@
 package neroxis.util;
 
+import map.FloatMask;
+
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
-import java.awt.image.BufferedImage;
+import java.awt.image.*;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class ImageUtils {
 
@@ -43,6 +47,72 @@ public class ImageUtils {
         imageScaled = scaleOp.filter(image, imageScaled);
 
         return imageScaled;
+    }
+
+    public static void writePNGFromMasks(FloatMask redMask, FloatMask greenMask, FloatMask blueMask, float scaleMultiplier, String path) throws IOException {
+        int size = redMask.getSize();
+        if (size != greenMask.getSize() || size != blueMask.getSize()) {
+            throw new IllegalArgumentException("Masks not the same size: redMask is " + redMask.getSize() +
+                    ", greenMask is " + greenMask.getSize() + " and blueMask is " + blueMask.getSize());
+        }
+        int length = size * size * 3;
+        int index = -1;
+        final byte[] byteArray = new byte[length];
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                byte redByte = (byte) ((Float) (redMask.getValueAt(x, y) * scaleMultiplier)).intValue();
+                byte greenByte = (byte) ((Float) (greenMask.getValueAt(x, y) * scaleMultiplier)).intValue();
+                byte blueByte = (byte) ((Float) (blueMask.getValueAt(x, y) * scaleMultiplier)).intValue();
+                index += 3;
+                byteArray[index - 2] = redByte;
+                byteArray[index - 1] = greenByte;
+                byteArray[index] = blueByte;
+            }
+        }
+        DataBuffer buffer = new DataBufferByte(byteArray, byteArray.length);
+        WritableRaster raster = Raster.createInterleavedRaster(buffer, size, size, 3 * size, 3, new int[]{0, 1, 2}, new Point(0, 0));
+        ColorModel colorModel = new ComponentColorModel(ColorModel.getRGBdefault().getColorSpace(), false, true, Transparency.TRANSLUCENT, DataBuffer.TYPE_BYTE);
+        RenderedImage image = (RenderedImage) new BufferedImage(colorModel, raster, true, null);
+        ImageIO.write(image, "png", new File(path));
+        System.out.println("PNG created at " + path);
+    }
+
+    public static void writeAutomaticallyScaledPNGFromMasks(FloatMask redMask, FloatMask greenMask, FloatMask blueMask, String path) throws IOException {
+        int size = redMask.getSize();
+        if (size != greenMask.getSize() || size != blueMask.getSize()) {
+            throw new IllegalArgumentException("Masks not the same size: redMask is " + redMask.getSize() +
+                    ", greenMask is " + greenMask.getSize() + " and blueMask is " + blueMask.getSize());
+        }
+        float scaleMultiplier = 255 / StrictMath.max(StrictMath.max(redMask.getMax(), greenMask.getMax()), blueMask.getMax());
+        int length = size * size * 3;
+        int index = -1;
+        final byte[] byteArray = new byte[length];
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                byte redByte = (byte) ((Float) (redMask.getValueAt(x, y) * scaleMultiplier)).intValue();
+                byte greenByte = (byte) ((Float) (greenMask.getValueAt(x, y) * scaleMultiplier)).intValue();
+                byte blueByte = (byte) ((Float) (blueMask.getValueAt(x, y) * scaleMultiplier)).intValue();
+                index += 3;
+                byteArray[index - 2] = redByte;
+                byteArray[index - 1] = greenByte;
+                byteArray[index] = blueByte;
+            }
+        }
+        DataBuffer buffer = new DataBufferByte(byteArray, byteArray.length);
+        WritableRaster raster = Raster.createInterleavedRaster(buffer, size, size, 3 * size, 3, new int[]{0, 1, 2}, new Point(0, 0));
+        ColorModel colorModel = new ComponentColorModel(ColorModel.getRGBdefault().getColorSpace(), false, true, Transparency.TRANSLUCENT, DataBuffer.TYPE_BYTE);
+        RenderedImage image = (RenderedImage) new BufferedImage(colorModel, raster, true, null);
+        ImageIO.write(image, "png", new File(path));
+        System.out.println("PNG created at " + path);
+    }
+
+    public static void writePNGFromMask(FloatMask mask, float scaleMultiplier, String path) throws IOException {
+        writePNGFromMasks(mask, mask, mask, scaleMultiplier, path);
+    }
+
+    public static void writeAutomaticallyScaledPNGFromMask(FloatMask mask, String path) throws IOException {
+        float scaleMultiplier = 255 / mask.getMax();
+        writePNGFromMasks(mask, mask, mask, scaleMultiplier, path);
     }
 
 }
