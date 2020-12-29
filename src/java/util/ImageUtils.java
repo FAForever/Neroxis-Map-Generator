e3656de7ba6dc292658d8d193a72f3eeb813824c
Emperor-Penguin1 <randompenguin27@gmail>
2020-12-29 14:17:35 -0500

    Resolved Issues

--- src/java/util/ImageUtils.java
+++ src/java/util/ImageUtils.java
@@ -56,17 +56,19 @@ public class ImageUtils {
                     ", greenMask is " + greenMask.getSize() + " and blueMask is " + blueMask.getSize());
         }
         int length = size * size * 3;
-        int index = -1;
+        int index = 0;
         final byte[] byteArray = new byte[length];
         for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
                 byte redByte = (byte) ((Float) (redMask.getValueAt(x, y) * scaleMultiplier)).intValue();
                 byte greenByte = (byte) ((Float) (greenMask.getValueAt(x, y) * scaleMultiplier)).intValue();
                 byte blueByte = (byte) ((Float) (blueMask.getValueAt(x, y) * scaleMultiplier)).intValue();
-                index += 3;
-                byteArray[index - 2] = redByte;
-                byteArray[index - 1] = greenByte;
+                byteArray[index] = redByte;
+                index += 1;
+                byteArray[index] = greenByte;
+                index += 1;
                 byteArray[index] = blueByte;
+                index += 1;
             }
         }
         DataBuffer buffer = new DataBufferByte(byteArray, byteArray.length);
@@ -77,40 +79,16 @@ public class ImageUtils {
         System.out.println("PNG created at " + path);
     }
 
-    public static void writeAutomaticallyScaledPNGFromMasks(FloatMask redMask, FloatMask greenMask, FloatMask blueMask, String path) throws IOException {
-        int size = redMask.getSize();
-        if (size != greenMask.getSize() || size != blueMask.getSize()) {
-            throw new IllegalArgumentException("Masks not the same size: redMask is " + redMask.getSize() +
-                    ", greenMask is " + greenMask.getSize() + " and blueMask is " + blueMask.getSize());
-        }
+    public static void writeAutoScaledPNGFromMasks(FloatMask redMask, FloatMask greenMask, FloatMask blueMask, String path) throws IOException {
         float scaleMultiplier = 255 / StrictMath.max(StrictMath.max(redMask.getMax(), greenMask.getMax()), blueMask.getMax());
-        int length = size * size * 3;
-        int index = -1;
-        final byte[] byteArray = new byte[length];
-        for (int x = 0; x < size; x++) {
-            for (int y = 0; y < size; y++) {
-                byte redByte = (byte) ((Float) (redMask.getValueAt(x, y) * scaleMultiplier)).intValue();
-                byte greenByte = (byte) ((Float) (greenMask.getValueAt(x, y) * scaleMultiplier)).intValue();
-                byte blueByte = (byte) ((Float) (blueMask.getValueAt(x, y) * scaleMultiplier)).intValue();
-                index += 3;
-                byteArray[index - 2] = redByte;
-                byteArray[index - 1] = greenByte;
-                byteArray[index] = blueByte;
-            }
-        }
-        DataBuffer buffer = new DataBufferByte(byteArray, byteArray.length);
-        WritableRaster raster = Raster.createInterleavedRaster(buffer, size, size, 3 * size, 3, new int[]{0, 1, 2}, new Point(0, 0));
-        ColorModel colorModel = new ComponentColorModel(ColorModel.getRGBdefault().getColorSpace(), false, true, Transparency.TRANSLUCENT, DataBuffer.TYPE_BYTE);
-        RenderedImage image = (RenderedImage) new BufferedImage(colorModel, raster, true, null);
-        ImageIO.write(image, "png", new File(path));
-        System.out.println("PNG created at " + path);
+        writePNGFromMasks(redMask, greenMask, blueMask, scaleMultiplier, path);
     }
 
     public static void writePNGFromMask(FloatMask mask, float scaleMultiplier, String path) throws IOException {
         writePNGFromMasks(mask, mask, mask, scaleMultiplier, path);
     }
 
-    public static void writeAutomaticallyScaledPNGFromMask(FloatMask mask, String path) throws IOException {
+    public static void writeAutoScaledPNGFromMask(FloatMask mask, String path) throws IOException {
         float scaleMultiplier = 255 / mask.getMax();
         writePNGFromMasks(mask, mask, mask, scaleMultiplier, path);
     }
