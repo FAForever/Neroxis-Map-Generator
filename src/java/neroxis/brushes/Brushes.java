147e2b9f561f73d8f991400c9aa7dd220cbb0243
Emperor-Penguin1 <randompenguin27@gmail>
2021-01-26 13:25:52 -0500

    Fixed Formatting Issue

--- src/java/neroxis/brushes/Brushes.java
+++ src/java/neroxis/brushes/Brushes.java
@@ -15,20 +15,20 @@ import java.util.List;
 public strictfp class Brushes {
     public static final List<String> MOUNTAIN_BRUSHES = Arrays.asList("mountain1.png", "mountain2.png", "mountain3.png", "mountain4.png", "mountain5.png",
             "mountain6.png", "mountain7.png", "mountain8.png", "mountain9.png", "volcano2.png", "hill1.png", "hill2.png", "noise1.png", "noise2.png",
-            "hawaii1.png", "hawaii2.png", "island.png", "volcano2.png", "Brush_1.png", "Brush_2.png", "Brush_3.png", "Brush_4.png", "Brush_5.png",
-            "Brush_6.png", "Brush_7.png", "Brush_8.png", "Brush_9.png", "Brush_10.png", "Brush_11.png", "Brush_12.png", "Brush_13.png", "Brush_14.png",
-            "Brush_15.png", "Brush_16.png", "Brush_17.png", "Brush_18.png", "Brush_19.png", "Brush_20.png", "Brush_21.png", "Brush_22.png", "Brush_23.png",
-            "Brush_24.png", "Brush_25.png", "Brush_26.png", "Brush_27.png", "Brush_28.png", "Brush_29.png", "Brush_30.png", "Brush_31.png", "Brush_32.png",
-            "Brush_33.png", "Brush_34.png", "Brush_35.png", "Brush_36.png", "Brush_37.png");
-    public static final List<String> HILL_BRUSHES = Arrays.asList("hill1.png", "hill2.png", "noise1.png", "noise2.png", "Brush_1.png", "Brush_2.png", "Brush_3.png", "Brush_16.png",
-            "Brush_21.png", "Brush_22.png", "Brush_23.png", "Brush_24.png", "Brush_25.png", "Brush_27.png", "Brush_29.png", "Brush_30.png", "Brush_31.png",
-            "Brush_32.png", "Brush_33.png", "Brush_35.png", "Brush_36.png");
+            "hawaii1.png", "hawaii2.png", "island.png", "volcano2.png", "Brush1.png", "Brush2.png", "Brush3.png", "Brush4.png", "Brush5.png",
+            "Brush6.png", "Brush7.png", "Brush8.png", "Brush9.png", "Brush10.png", "Brush11.png", "Brush12.png", "Brush13.png", "Brush14.png",
+            "Brush15.png", "Brush16.png", "Brush17.png", "Brush18.png", "Brush19.png", "Brush20.png", "Brush21.png", "Brush22.png", "Brush23.png",
+            "Brush24.png", "Brush25.png", "Brush26.png", "Brush27.png", "Brush28.png", "Brush29.png", "Brush30.png", "Brush31.png", "Brush32.png",
+            "Brush33.png", "Brush34.png", "Brush35.png", "Brush36.png", "Brush37.png");
+    public static final List<String> HILL_BRUSHES = Arrays.asList("hill1.png", "hill2.png", "noise1.png", "noise2.png", "Brush1.png", "Brush2.png", "Brush3.png", "Brush16.png",
+            "Brush21.png", "Brush22.png", "Brush23.png", "Brush24.png", "Brush25.png", "Brush27.png", "Brush29.png", "Brush30.png", "Brush31.png",
+            "Brush32.png", "Brush33.png", "Brush35.png", "Brush36.png");
     public static final List<String> GENERATOR_BRUSHES = Arrays.asList("mountain1.png", "mountain2.png", "mountain4.png",
-            "mountain5.png", "mountain6.png", "volcano2.png", "Brush_1.png", "Brush_2.png", "Brush_3.png", "Brush_4.png", "Brush_5.png",
-            "Brush_6.png", "Brush_7.png", "Brush_8.png", "Brush_9.png", "Brush_10.png", "Brush_11.png", "Brush_12.png", "Brush_13.png", "Brush_14.png",
-            "Brush_15.png", "Brush_16.png", "Brush_17.png", "Brush_18.png", "Brush_19.png", "Brush_20.png", "Brush_21.png", "Brush_22.png", "Brush_23.png",
-            "Brush_24.png", "Brush_25.png", "Brush_26.png", "Brush_27.png", "Brush_28.png", "Brush_29.png", "Brush_30.png", "Brush_31.png", "Brush_32.png",
-            "Brush_33.png", "Brush_34.png", "Brush_35.png", "Brush_36.png", "Brush_37.png");
+            "mountain5.png", "mountain6.png", "volcano2.png", "Brush1.png", "Brush2.png", "Brush3.png", "Brush4.png", "Brush5.png",
+            "Brush6.png", "Brush7.png", "Brush8.png", "Brush9.png", "Brush10.png", "Brush11.png", "Brush12.png", "Brush13.png", "Brush14.png",
+            "Brush15.png", "Brush16.png", "Brush17.png", "Brush18.png", "Brush19.png", "Brush20.png", "Brush21.png", "Brush22.png", "Brush23.png",
+            "Brush24.png", "Brush25.png", "Brush26.png", "Brush27.png", "Brush28.png", "Brush29.png", "Brush30.png", "Brush31.png", "Brush32.png",
+            "Brush33.png", "Brush34.png", "Brush35.png", "Brush36.png", "Brush37.png");
     public static final String CUSTOM_BRUSHES_DIR = "/images/brushes/";
 
     public static FloatMask loadBrush(String brushPath, Long seed) {
