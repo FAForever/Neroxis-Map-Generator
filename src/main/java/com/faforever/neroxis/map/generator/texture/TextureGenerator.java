55a549112e5d80634bd7e1f235e819d1edc5baec
Sheikah45 <Sheikah450@gmail.com>
2021-05-26 06:13:30 -0400

    Decrease normals instensity

--- src/main/java/com/faforever/neroxis/map/generator/texture/TextureGenerator.java
+++ src/main/java/com/faforever/neroxis/map/generator/texture/TextureGenerator.java
@@ -18,8 +18,8 @@ public abstract strictfp class TextureGenerator extends ElementGenerator {
         super.initialize(map, seed, mapParameters);
         heightmap = terrainGenerator.getHeightmap();
         slope = terrainGenerator.getSlope();
-        normals = heightmap.copy().resample(512).addPerlinNoise(64, 8f).blur(1)
-                .addGaussianNoise(.01f).getNormalMask(8f);
+        normals = heightmap.copy().resample(512).addPerlinNoise(64, 12f).blur(1)
+                .addGaussianNoise(.01f).getNormalMask(4f);
     }
 
     public abstract void setTextures();
