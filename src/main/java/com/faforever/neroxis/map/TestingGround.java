116b0f381bb51ff37f3580d37e6c034e8d5b210f
Sheikah45 <Sheikah450@gmail.com>
2021-07-03 19:52:52 -0400

    Add testing ground

---
+++ src/main/java/com/faforever/neroxis/map/TestingGround.java
@@ -0,0 +1,13 @@
+package com.faforever.neroxis.map;
+
+import com.faforever.neroxis.util.Util;
+
+public strictfp class TestingGround {
+
+    public static void main(String[] args) throws Exception {
+        Util.DEBUG = true;
+        Util.VISUALIZE = true;
+
+
+    }
+}
