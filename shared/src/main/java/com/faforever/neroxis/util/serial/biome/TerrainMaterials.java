aa951321160811e5b8470bb1381ab88cb79d29f0
BlackYps <jonas-fb@gmx.de>
2023-03-25 13:31:11 +0100

    Initialize cubemap list

--- shared/src/main/java/com/faforever/neroxis/util/serial/biome/TerrainMaterials.java
+++ shared/src/main/java/com/faforever/neroxis/util/serial/biome/TerrainMaterials.java
@@ -4,6 +4,7 @@ import com.dslplatform.json.CompiledJson;
 import com.faforever.neroxis.map.CubeMap;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -13,7 +14,7 @@ public class TerrainMaterials {
     public static final int TERRAIN_TEXTURE_COUNT = 10;
     public static final int TERRAIN_NORMAL_COUNT = 9;
     private String name;
-    private List<CubeMap> cubeMaps;
+    private List<CubeMap> cubeMaps = new ArrayList<>();
     private String[] texturePaths = new String[TERRAIN_TEXTURE_COUNT];
     private float[] textureScales = new float[TERRAIN_TEXTURE_COUNT];
     private String[] normalPaths = new String[TERRAIN_NORMAL_COUNT];
