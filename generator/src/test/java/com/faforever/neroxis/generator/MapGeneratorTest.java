2479a2d5bf5698a4ed019a805ae0b857b70ddb5e
Sheikah45 <Sheikah450@gmail.com>
2023-03-24 23:08:54 -0400

    Disable flaky test

--- generator/src/test/java/com/faforever/neroxis/generator/MapGeneratorTest.java
+++ generator/src/test/java/com/faforever/neroxis/generator/MapGeneratorTest.java
@@ -12,6 +12,7 @@ import com.faforever.neroxis.util.MathUtil;
 import com.faforever.neroxis.util.Pipeline;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.parallel.Execution;
@@ -157,6 +158,7 @@ public class MapGeneratorTest {
 
     @ParameterizedTest
     @ArgumentsSource(ValidMapSizeArgumentProvider.class)
+    @Disabled("Flaky Test, OOM")
     public void TestMapExportedToProperSize(int mapSize) {
         new CommandLine(instance).execute("--map-size", String.valueOf(mapSize));
 
