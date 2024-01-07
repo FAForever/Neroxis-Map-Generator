75ae1cc3d95eef679f422966eddc75aafa9a6dd6
BlackYps <jonas-fb@gmx.de>
2024-01-07 02:09:11 +0100

    Add boulder reclaim fields

--- generator/src/main/java/com/faforever/neroxis/generator/prop/HighReclaimPropGenerator.java
+++ generator/src/main/java/com/faforever/neroxis/generator/prop/HighReclaimPropGenerator.java
@@ -12,6 +12,8 @@ import com.faforever.neroxis.util.Pipeline;
 
 public class HighReclaimPropGenerator extends BasicPropGenerator {
     protected BooleanMask fieldBoulderMask;
+    protected BooleanMask boulderReclaimAreaMask;
+    protected BooleanMask stoneReclaimAreaMask;
 
     @Override
     public ParameterConstraints getParameterConstraints() {
@@ -28,11 +30,13 @@ public class HighReclaimPropGenerator extends BasicPropGenerator {
                            SymmetrySettings symmetrySettings, TerrainGenerator terrainGenerator) {
         super.initialize(map, seed, generatorParameters, symmetrySettings, terrainGenerator);
         fieldBoulderMask = new BooleanMask(1, random.nextLong(), symmetrySettings, "fieldBoulderMask", true);
+        boulderReclaimAreaMask = new BooleanMask(1, random.nextLong(), symmetrySettings, "boulderReclaimAreaMask", true);
+        stoneReclaimAreaMask = new BooleanMask(1, random.nextLong(), symmetrySettings, "stoneReclaimAreaMask", true);
     }
 
     @Override
     public void placePropsWithExclusion() {
-        Pipeline.await(treeMask, cliffRockMask, fieldStoneMask, fieldBoulderMask);
+        Pipeline.await(treeMask, cliffRockMask, fieldStoneMask, fieldBoulderMask, stoneReclaimAreaMask, boulderReclaimAreaMask);
         DebugUtil.timedRun("com.faforever.neroxis.map.generator", "placeProps", () -> {
             Biome biome = generatorParameters.biome();
             propPlacer.placeProps(treeMask.getFinalMask().subtract(noProps), biome.propMaterials().getTreeGroups(),
@@ -41,7 +45,11 @@ public class HighReclaimPropGenerator extends BasicPropGenerator {
             propPlacer.placeProps(fieldStoneMask.getFinalMask().subtract(noProps),
                                   biome.propMaterials().getRocks(), .5f, 2.5f);
             propPlacer.placeProps(fieldBoulderMask.getFinalMask().subtract(noProps),
-                                  biome.propMaterials().getBoulders(), 15f);
+                                  biome.propMaterials().getBoulders(), 5f, 10f);
+            propPlacer.placeProps(stoneReclaimAreaMask.getFinalMask().subtract(noProps),
+                                  biome.propMaterials().getRocks(), 1f, 3f);
+            propPlacer.placeProps(boulderReclaimAreaMask.getFinalMask().subtract(noProps),
+                                  biome.propMaterials().getBoulders(), 3f, 5f);
         });
     }
 
@@ -52,8 +60,16 @@ public class HighReclaimPropGenerator extends BasicPropGenerator {
         int spawnCount = generatorParameters.spawnCount();
         treeMask.setSize(mapSize / 16);
         cliffRockMask.setSize(mapSize / 32);
-        fieldStoneMask.setSize(mapSize / 4);
         fieldBoulderMask.setSize(mapSize / 4);
+        boulderReclaimAreaMask.setSize(mapSize / 4);
+        
+        BooleanMask reclaimArea = new BooleanMask(1, random.nextLong(), symmetrySettings, "reclaimArea", true);
+        reclaimArea.setSize(mapSize / 4);
+        reclaimArea.randomize(reclaimDensity * spawnCount * .0005f).dilute(.8f, 4).setSize(mapSize + 1);
+        boulderReclaimAreaMask.randomize(0.5f).setSize(mapSize + 1).multiply(reclaimArea);
+        stoneReclaimAreaMask.init(boulderReclaimAreaMask).dilute(.5f, 2).subtract(boulderReclaimAreaMask).dilute(.5f);
+        boulderReclaimAreaMask.multiply(passableLand).fillEdge(10, false);
+        stoneReclaimAreaMask.multiply(passableLand).fillEdge(9, false);
 
         cliffRockMask.randomize(reclaimDensity * .5f).setSize(mapSize + 1);
         cliffRockMask.multiply(impassable).dilute(.5f, 10).subtract(impassable).multiply(passableLand);
@@ -63,6 +79,7 @@ public class HighReclaimPropGenerator extends BasicPropGenerator {
         treeMask.randomize((reclaimDensity + random.nextFloat()) / 2f * .15f).setSize(mapSize / 4);
         treeMask.inflate(2).erode(.5f);
         treeMask.setSize(mapSize + 1);
+        treeMask.subtract(boulderReclaimAreaMask).subtract(stoneReclaimAreaMask.copy().dilute(.8f, 2)).subtract(fieldBoulderMask);
         treeMask.multiply(passableLand.copy().deflate(8)).fillEdge(8, false);
     }
 }
