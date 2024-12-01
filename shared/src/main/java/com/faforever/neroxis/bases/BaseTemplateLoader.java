2ee476ccb6dcd7db10778f15dddf2bf0d016614a
Sheikah45 <Sheikah450@gmail.com>
2024-12-01 16:39:01 -0500

    Convert ScenarioImporter

--- shared/src/main/java/com/faforever/neroxis/bases/BaseTemplateLoader.java
+++ shared/src/main/java/com/faforever/neroxis/bases/BaseTemplateLoader.java
@@ -11,7 +11,6 @@ import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.SequencedCollection;
 import java.util.SequencedMap;
 import java.util.SequencedSet;
 import java.util.stream.Collectors;
