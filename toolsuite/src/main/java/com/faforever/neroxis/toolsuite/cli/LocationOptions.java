fea3df15cc1dd6f8d5458b03fbc1d55a08d5def5
Sheikah45 <Sheikah450@gmail.com>
2024-12-07 22:22:58 -0500

    Fix location options

--- toolsuite/src/main/java/com/faforever/neroxis/toolsuite/cli/LocationOptions.java
+++ toolsuite/src/main/java/com/faforever/neroxis/toolsuite/cli/LocationOptions.java
@@ -1,20 +1,21 @@
 package com.faforever.neroxis.toolsuite.cli;
 
+import com.faforever.neroxis.util.vector.Vector;
 import com.faforever.neroxis.util.vector.Vector2;
 import lombok.Getter;
 import picocli.CommandLine;
 
 @Getter
 public class LocationOptions {
-    private final Vector2 location = new Vector2();
+    private Vector2 location = new Vector2();
 
     @CommandLine.Option(names = "--x", required = true, description = "x-coordinate")
     public void setX(float x) {
-        location.setX(x);
+        location = location.withComponent(Vector.X, x);
     }
 
     @CommandLine.Option(names = "--y", required = true, description = "y-coordinate")
     public void setY(float y) {
-        location.setY(y);
+        location = location.withComponent(Vector.Y, y);
     }
 }
