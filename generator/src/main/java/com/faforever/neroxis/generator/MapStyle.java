c56e0f77a66d3d4880beb8d36e038d3ecd0841ba
Clive Paterson <clive.paterson@gmail.com>
2025-04-24 19:29:54 +0800

    Changes to rivers terrain gen (#441)
    * Time to pr some fixes to the RiversTerrainGenerator, this focuses on some essential improvements to the rivers gen, and reduces its prevalence on ladder.
    
    * Was a bit mean to the River's... just re-balancing things.
    
    * Tweaks to the mountain exclusion mask.

--- generator/src/main/java/com/faforever/neroxis/generator/MapStyle.java
+++ generator/src/main/java/com/faforever/neroxis/generator/MapStyle.java
@@ -27,7 +27,7 @@ import java.util.function.Supplier;
 @AllArgsConstructor
 public enum MapStyle {
     BASIC(BasicStyleGenerator.class, BasicStyleGenerator::new, 1),
-    BIG_ISLANDS(BigIslandsStyleGenerator.class, BigIslandsStyleGenerator::new, 4),
+    BIG_ISLANDS(BigIslandsStyleGenerator.class, BigIslandsStyleGenerator::new, 1),
     CENTER_LAKE(CenterLakeStyleGenerator.class, CenterLakeStyleGenerator::new, 1),
     DROP_PLATEAU(DropPlateauStyleGenerator.class, DropPlateauStyleGenerator::new, .5f),
     FLOODED(FloodedStyleGenerator.class, FloodedStyleGenerator::new, .01f),
@@ -36,13 +36,13 @@ public enum MapStyle {
     LITTLE_MOUNTAIN(LittleMountainStyleGenerator.class, LittleMountainStyleGenerator::new, 1),
     LOW_MEX(LowMexStyleGenerator.class, LowMexStyleGenerator::new, .5f),
     MOUNTAIN_RANGE(MountainRangeStyleGenerator.class, MountainRangeStyleGenerator::new, 1),
-    MULTILEVEL(MultiLevelStyleGenerator.class, MultiLevelStyleGenerator::new, 1),
-    FLOODED_MULTILEVEL(FloodedMultiLevelStyleGenerator.class, FloodedMultiLevelStyleGenerator::new, 1),
+    MULTILEVEL(MultiLevelStyleGenerator.class, MultiLevelStyleGenerator::new, 1f),
+    FLOODED_MULTILEVEL(FloodedMultiLevelStyleGenerator.class, FloodedMultiLevelStyleGenerator::new, 0.75f),
     ONE_ISLAND(OneIslandStyleGenerator.class, OneIslandStyleGenerator::new, 1),
-    SMALL_ISLANDS(SmallIslandsStyleGenerator.class, SmallIslandsStyleGenerator::new, 4),
+    SMALL_ISLANDS(SmallIslandsStyleGenerator.class, SmallIslandsStyleGenerator::new, 1),
     VALLEY(ValleyStyleGenerator.class, ValleyStyleGenerator::new, 1),
-    RIVERS(RiversStyleGenerator.class, RiversStyleGenerator::new, 1),
-    RIVERS_AND_OCEANS(RiversAndOceansStyleGenerator.class, RiversAndOceansStyleGenerator::new, 1);
+    RIVERS(RiversStyleGenerator.class, RiversStyleGenerator::new, 0.25f),
+    RIVERS_AND_OCEANS(RiversAndOceansStyleGenerator.class, RiversAndOceansStyleGenerator::new, 0.75f);
 
     private final Class<? extends StyleGenerator> generatorClass;
     private final Supplier<StyleGenerator> generatorSupplier;
--- generator/src/main/java/com/faforever/neroxis/generator/terrain/RiversTerrainGenerator.java
+++ generator/src/main/java/com/faforever/neroxis/generator/terrain/RiversTerrainGenerator.java
@@ -28,7 +28,9 @@ public class RiversTerrainGenerator extends BasicTerrainGenerator {
 
         mountainBrushSize = 24;
         mountainBrushDensity = 8f;
-        mountainBrushIntensity = 0.4f;
+        mountainBrushIntensity = 0.8f;
+
+        spawnSize = 48;
     }
 
     @Override
@@ -42,17 +44,10 @@ public class RiversTerrainGenerator extends BasicTerrainGenerator {
         rivers.addPerlinNoise(StrictMath.min(96 + riversScale, mapSize), 1);
         riverMask = rivers.copyAsBooleanMask(0.5f, 0.65f);
 
-        FloatMask riverExclusion = new FloatMask(mapSize, getRandom().nextLong(), land.getSymmetrySettings(), "riversExclusion", true);
-        riverExclusion.addPerlinNoise(StrictMath.min(256, mapSize), 1);
-        float exclusionThickness = 0.06f * ((float)mapSize / 256f);
-        riverExclusionMask = riverExclusion.copyAsBooleanMask(0.5f, 0.5f + exclusionThickness);
-        riverMask.subtract(riverExclusionMask);
-        riverExclusionMask.setSize(mapSize+1);
-
         riverMask.invert();
         riverMask.blur(10);
 
-        riverMask.add(connections.copy().dilute(1, 10).setSize(riverMask.getSize()));
+        riverMask.add(connections.copy().dilute(1, 20).setSize(riverMask.getSize()));
 
         riverMask.erode(0.3f, 10);
 
@@ -184,8 +179,8 @@ public class RiversTerrainGenerator extends BasicTerrainGenerator {
                                   .inflate(2);
 
         FloatMask riverMountainExclusion = new FloatMask(mapSize, random.nextLong(), this.symmetrySettings, "riverMountainExclusion", true);
-        riverMountainExclusion.addPerlinNoise(128, 1);
-        BooleanMask riverMountainExclusionMask = riverMountainExclusion.copyAsBooleanMask(0.3f, 0.8f);
+        riverMountainExclusion.addPerlinNoise(64, 1);
+        BooleanMask riverMountainExclusionMask = riverMountainExclusion.copyAsBooleanMask(0.3f, 0.75f);
 
         riverMountains.subtract(riverMountainExclusionMask);
         riverMountains.setSize(mountains.getSize());
@@ -219,7 +214,7 @@ public class RiversTerrainGenerator extends BasicTerrainGenerator {
     protected void spawnMaskSetup() {
         map.getSpawns().forEach(spawn -> {
             Vector3 location = spawn.getPosition();
-            spawnLandMask.fillCircle(location, 10, true);
+            spawnLandMask.fillCircle(location, spawnSize, true);
         });
     }
 
