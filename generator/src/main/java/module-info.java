5beaad6a166b6e124dc6c794aecebbc4eea7d934
Sheikah45 <Sheikah450@gmail.com>
2025-06-15 12:22:00 -0400

    Remove unneeded adapters

--- generator/src/main/java/module-info.java
+++ generator/src/main/java/module-info.java
@@ -1,8 +1,7 @@
 module com.faforever.neroxis.generator {
+    requires info.picocli;
     requires com.faforever.neroxis.shared;
     requires org.apache.commons.codec;
-    requires static lombok;
 
-    opens com.faforever.neroxis.generator to info.picocli;
-    opens com.faforever.neroxis.generator.cli to info.picocli;
+    requires static lombok;
 }
--- shared/src/main/java/module-info.java
+++ shared/src/main/java/module-info.java
@@ -1,24 +1,24 @@
 module com.faforever.neroxis.shared {
-    requires transitive java.desktop;
-    requires transitive info.picocli;
-    requires org.antlr.antlr4.runtime;
-    requires io.avaje.jsonb;
-    requires static lombok;
-
-    exports com.faforever.neroxis.util;
     exports com.faforever.neroxis.cli;
-    exports com.faforever.neroxis.exporter;
-    exports com.faforever.neroxis.map;
     exports com.faforever.neroxis.util.vector;
+    exports com.faforever.neroxis.map;
     exports com.faforever.neroxis.mask;
-    exports com.faforever.neroxis.biomes;
-    exports com.faforever.neroxis.bases;
-    exports com.faforever.neroxis.brushes;
+    exports com.faforever.neroxis.exporter;
     exports com.faforever.neroxis.importer;
+    exports com.faforever.neroxis.util;
+    exports com.faforever.neroxis.biomes;
     exports com.faforever.neroxis.map.placement;
     exports com.faforever.neroxis.util.serial.biome;
+    exports com.faforever.neroxis.brushes;
+    exports com.faforever.neroxis.bases;
 
-    provides io.avaje.jsonb.spi.JsonbExtension with com.faforever.neroxis.util.jsonb.GeneratedJsonComponent;
+    requires io.avaje.jsonb;
+    requires org.antlr.antlr4.runtime;
+    requires info.picocli;
 
-    opens com.faforever.neroxis.cli to info.picocli;
+    requires transitive java.desktop;
+
+    requires static lombok;
+
+    provides io.avaje.jsonb.spi.JsonbExtension with com.faforever.neroxis.jsonb.GeneratedJsonComponent;
 }
