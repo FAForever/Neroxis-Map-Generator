d800e5c4ad38ce1eb9cc8e23abc3c77ab8d68059
clive <clive.paterson@gmail.com>
2025-09-03 22:25:31 +0800

    Some small adjustments to the crystalline biome and multi level terrain generators.

--- generator/src/main/java/com/faforever/neroxis/generator/style/MultiLevelStyleGenerator.java
+++ generator/src/main/java/com/faforever/neroxis/generator/style/MultiLevelStyleGenerator.java
@@ -25,13 +25,12 @@ public class MultiLevelStyleGenerator extends StyleGenerator {
     protected WeightedOptionsWithFallback<PropGenerator> getPropGeneratorOptions() {
         return WeightedOptionsWithFallback.of(new BasicPropGenerator(),
                                               new WeightedOption<>(new BasicPropGenerator(), 1f),
-                                              new WeightedOption<>(new BoulderFieldPropGenerator(), .1f),
+                                              new WeightedOption<>(new BoulderFieldPropGenerator(), 1f),
                                               new WeightedOption<>(new EnemyCivPropGenerator(), .5f),
                                               new WeightedOption<>(new HighReclaimPropGenerator(), .25f),
                                               new WeightedOption<>(new LargeBattlePropGenerator(), .5f),
-                                              new WeightedOption<>(new NavyWrecksPropGenerator(), 2f),
+                                              new WeightedOption<>(new SmallBattlePropGenerator(), .5f),
                                               new WeightedOption<>(new NeutralCivPropGenerator(), 1f),
-                                              new WeightedOption<>(new RockFieldPropGenerator(), 1f),
-                                              new WeightedOption<>(new SmallBattlePropGenerator(), 1f));
+                                              new WeightedOption<>(new RockFieldPropGenerator(), 1f));
     }
 }
