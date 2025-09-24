90a632c91738891c11f3bd21bf3c6fb78d32754f
clive <clive.paterson@gmail.com>
2025-09-24 22:06:21 +0800

    greatly reduce the likelihood of water mexes.

--- generator/src/main/java/com/faforever/neroxis/generator/resource/RiversAndOceansMexResourceGenerator.java
+++ generator/src/main/java/com/faforever/neroxis/generator/resource/RiversAndOceansMexResourceGenerator.java
@@ -1,15 +1,34 @@
 package com.faforever.neroxis.generator.resource;
 
+import com.faforever.neroxis.generator.GeneratorParameters;
+import com.faforever.neroxis.generator.terrain.TerrainGenerator;
+import com.faforever.neroxis.map.SCMap;
+import com.faforever.neroxis.map.SymmetrySettings;
+import com.faforever.neroxis.mask.FloatMask;
+import com.faforever.neroxis.util.Pipeline;
+
 public class RiversAndOceansMexResourceGenerator extends BasicResourceGenerator {
+
+    private FloatMask waterResourceLimitNoiseMask;
+
+    public void initialize(SCMap map, long seed, GeneratorParameters generatorParameters,
+                           SymmetrySettings symmetrySettings, TerrainGenerator terrainGenerator, Pipeline pipeline) {
+        super.initialize(map, seed, generatorParameters, symmetrySettings, terrainGenerator, pipeline);
+
+        waterResourceLimitNoiseMask = new FloatMask(1, random.nextLong(), symmetrySettings, "waterResourceLimitNoiseMask", pipeline);
+    }
 
     @Override
     public void setupPipeline() {
+        waterResourceLimitNoiseMask.setSize(passableLand.getSize());
+        waterResourceLimitNoiseMask.addWhiteNoise(0, 1);
+
         resourceMask.init(passableLand);
         resourceMask.add(passableWater
                                  .copy()
-                                 .acid(random.nextFloat(0.3f, 0.7f), 0)
-                                 .erode(random.nextFloat(0.5f,0.7f)));
+                                 .subtract(waterResourceLimitNoiseMask.copyAsBooleanMask(0.01f)));
         resourceMask.subtract(unbuildable.copy().inflate(2));
-        waterResourceMask.init(resourceMask);
+
+        waterResourceMask.setSize(resourceMask.getSize());
     }
 }
