6d3b1975dc6d6c73355675df5f068ed27691f28e
clive <clive.paterson@gmail.com>
2025-11-23 19:49:10 +0800

    Change the team separation for spawn last

--- shared/src/main/java/com/faforever/neroxis/map/placement/SpawnPlacer.java
+++ shared/src/main/java/com/faforever/neroxis/map/placement/SpawnPlacer.java
@@ -81,10 +81,23 @@ public class SpawnPlacer {
 
     private boolean tryPlaceSpawns(int spawnCount, BooleanMask spawnMask, float teammateSeparation,
                                    int teamSeparation) {
+        Symmetry spawnSymmetry = spawnMask.getSymmetrySettings().spawnSymmetry();
+        int mapSize = map.getSize();
+
+        BooleanMask teamSeparationMask = new BooleanMask(map.getSize() + 1, random.nextLong(),
+                                                         spawnMask.getSymmetrySettings());
+        teamSeparationMask.drawSymmetryLines();
+        if (spawnSymmetry.isPerfectSymmetry()) {
+            teamSeparationMask.inflate(teamSeparation / 2f);
+        } else {
+            teamSeparationMask.inflate((float) teamSeparation / spawnSymmetry.getNumSymPoints())
+                              .fillCircle(mapSize / 2f, mapSize / 2f, mapSize / 4f, true);
+        }
+
         BooleanMask spawnMaskCopy = spawnMask.copy();
-        spawnMaskCopy.fillSides(map.getSize() / spawnCount * 3 / 2, false)
-                     .fillCenter(teamSeparation, false)
-                     .fillEdge(map.getSize() / 32, false)
+        spawnMaskCopy.fillSides(mapSize / spawnCount * 3 / 2, false)
+                     .subtract(teamSeparationMask)
+                     .fillEdge(mapSize / 32, false)
                      .limitToSymmetryRegion();
         Vector2 location = spawnMaskCopy.getRandomPosition();
         while (map.getSpawnCount() < spawnCount) {
