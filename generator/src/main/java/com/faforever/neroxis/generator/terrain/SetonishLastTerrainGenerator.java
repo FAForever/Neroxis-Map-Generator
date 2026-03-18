b61c0cce31813e581850f41cf1d9a2f9e091da2a
clive <clive.paterson@gmail.com>
2026-03-18 21:34:35 +0800

    remove visual debugger

--- generator/src/main/java/com/faforever/neroxis/generator/terrain/SetonishLastTerrainGenerator.java
+++ generator/src/main/java/com/faforever/neroxis/generator/terrain/SetonishLastTerrainGenerator.java
@@ -160,7 +160,7 @@ public class SetonishLastTerrainGenerator extends FractalNoiseLastTerrainGenerat
 
     @Override
     protected void blurRamps() {
-        BooleanMask inflatedRamps = ramps.copy().startVisualDebugger();
+        BooleanMask inflatedRamps = ramps.copy();
 
         heightmap.blur(4, inflatedRamps.copy().inflate(4))
                  .blur(4, inflatedRamps.copy().inflate(4).outline().inflate(2))
