2c53a4b9c8d831f94600c04217572049ef1831fd
clive <clive.paterson@gmail.com>
2026-07-03 21:25:41 +0800

    unused import

--- generator/src/main/java/com/faforever/neroxis/generator/style/MultiLevelStyleGenerator.java
+++ generator/src/main/java/com/faforever/neroxis/generator/style/MultiLevelStyleGenerator.java
@@ -8,7 +8,6 @@ import com.faforever.neroxis.generator.prop.EnemyCivPropGenerator;
 import com.faforever.neroxis.generator.prop.HeatMapPropGenerator;
 import com.faforever.neroxis.generator.prop.HighReclaimPropGenerator;
 import com.faforever.neroxis.generator.prop.LargeBattlePropGenerator;
-import com.faforever.neroxis.generator.prop.NavyWrecksPropGenerator;
 import com.faforever.neroxis.generator.prop.NeutralCivPropGenerator;
 import com.faforever.neroxis.generator.prop.PropGenerator;
 import com.faforever.neroxis.generator.prop.RockFieldPropGenerator;
